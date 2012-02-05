ffed1bafe2baa23db5420fd6bec1ddcdb2ec56a1
Chris Goetz <goetzcj@gmail.com>
2012-02-05 08:40:17 -0600

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shortsy2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string linkid)
+        {
+            Response.StatusCode = 302;
+            Response.RedirectLocation = (string.IsNullOrWhiteSpace(linkid)
+                    ? "http://www.aspireforit.com"
+                    : string.Format("http://bit.ly/{0}", linkid));
+            return new ContentResult();
+            //This is a test
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Shortsy2.MvcApplication" Language="C#" %>
